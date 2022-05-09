b043f5e60b16923b550d6cf120b74692bf006adb
karthikred1 <101375670+karthikred1@users.noreply.github.com>
2022-05-09 10:05:01 +0530

    Update Test1.java

--- Test1.java
+++ Test1.java
@@ -1,7 +1,8 @@
 class Test1 {
 	public static void main(String[] args) {
 		String str1 = "Test1";
-		String str7 = "test8";
+		String str7 = "test7";
+		String str9 = "test9";
 		String str6 = "Test6";
     String str2 = "Test2";
   	String str3 = "Test3";
